153135016f5b30cae3089479248703ebf3c4f7bb
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-02-13 12:28:18 -0300

    Add files via upload

---
+++ euler_5.py
@@ -0,0 +1,23 @@
+# Smallest multiple
+''' 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 
+without any remainder. What is the smallest positive number that is evenly divisible by 
+all of the numbers from 1 to 20?'''
+
+
+def divisivel(lista, num):
+    for i in lista:
+        if num % i != 0:
+            return False
+        else:
+            continue
+    return True
+
+
+lista = list(range(1, 21))
+n = 2520
+while True:
+    if divisivel(lista, n):
+        print(n)
+        break
+    else:
+        n += 2520
