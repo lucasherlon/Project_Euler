feaeb30f944ce37cdc449d2f2d929b3cc42c08af
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-02-13 16:44:51 -0300

    Update euler_6.py

--- euler_6.py
+++ euler_6.py
@@ -1,5 +1,9 @@
 # Sum square difference
+'''Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.'''
+
 from math import pow
+
 lista = list(range(1, 101))
 soma = sum(lista)
 soma_q = pow(soma, 2)  # quadrado da soma dos números da lista
