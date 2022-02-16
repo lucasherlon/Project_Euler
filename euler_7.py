6439655a857abbed6ac286787a71d010bbe14648
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-02-16 18:06:11 -0300

    Update euler_7.py

--- euler_7.py
+++ euler_7.py
@@ -1,5 +1,6 @@
 #10001st prime
-'''By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13. What is the 10 001st prime number?'''
+'''By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13. 
+What is the 10 001st prime number?'''
 
 
 def primo(n):
