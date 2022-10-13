de7ed697cd4a470a2b9a8796b518eeee48811dfc
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-10-13 10:08:20 -0300

    Euler_9 solution
    Note: The rule about sharing solutions outside of Project Euler does not apply to the first one-hundred problems.

---
+++ euler_9.py
@@ -0,0 +1,24 @@
+'''
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a2 + b2 = c2
+
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+
+def triplet():
+    for i in range(0,1001):
+        for j in range(0,1001):
+            for k in range(0,1001):
+                if i<j and j<k:
+                    if(i*i+j*j == k*k):
+                        if(i+k+j == 1000):
+                            prod = i*j*k
+                            return prod
+
+res = triplet()
+
+print(res)
